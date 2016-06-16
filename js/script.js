fdb7519cf17ab24bb3c68175fe01853e563f296b
Connor Bracewell <cbrace@uvic.ca>
2016-06-15 19:41:04 -0700

    added advanced navigation and no-js fallbacks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 $(document).ready( function() {
-	//show only the first content pane
+	//show only the selected (default first) content pane
 	$(".content-container").children().addClass("hidden");
-	$($(".navigation-list a").first().attr("data-show")).removeClass("hidden");
-	
+	initialShow = "";
+	if (window.location.hash === "") {
+		initialShow = $(".navigation-list a").first().attr("data-show");
+	} else {
+		initialShow = $(".navigation-list a[href=\"" + window.location.hash + "\"]").attr("data-show");
+	}
+	$(initialShow).removeClass("hidden");
+
+	//hide the fallback headers
+	$(".fallback-header").each(function(){ $(this).addClass("hidden"); });
+
 	//add click events to the navigation bar
 	$(".navigation-list a").click( function(e) {
 		$(".content-container").children().addClass("hidden");
 		$($(e.target).attr("data-show")).removeClass("hidden");
+		history.replaceState(
+			null,
+			document.title,
+			window.location.pathname + $(e.target).attr("href")
+		);
 		e.preventDefault();
 	});
 });
