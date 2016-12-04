f37e7af3e52d258e868ff42dab47a16cd99bf4a8
Connor Bracewell <cbrace@uvic.ca>
2016-12-03 17:47:01 -0800

    Fixed a bug on the uploads page.

--- uploads/index.php
+++ uploads/index.php
@@ -10,7 +10,7 @@ function print_dir_listing($dir) {
     $hidden = array(".", ".htaccess", "index.php");
     $files = array_diff($files, $hidden);
     foreach ($files as $file) {
-        if (explode(".", $file)[-1] == "mp3") {
+        if (explode(".",$file)[count($files)-1] == "mp3") {
             echo("<audio controls><source src=\"" . $file . "\" type=\"audio/mpeg\"></audio> (" . $file . ")<br />");
         } else {
             echo("<a href=\"" . $file . "\">" . $file . "</a><br />");
