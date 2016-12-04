f3e5ed825ac05aad100e96075e9fc1e6f78492ae
Connor Bracewell <cbrace@uvic.ca>
2016-12-03 17:10:46 -0800

    Replaced tabs for spaces in all files.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
 $(document).ready( function() {
-	//hide fallback content and show script content
-	$(".js-only").prop("hidden", false);
-	$(".nojs-only").prop("hidden", true);
+    //hide fallback content and show script content
+    $(".js-only").prop("hidden", false);
+    $(".nojs-only").prop("hidden", true);
 
-	//update all the container IDs (so they don't match the url hashes anymore)
-	//ie. #resume becomes #panel-resume
-	$(".navigation-list a").each(function(){
-		var target = $(this);
-		$(target.attr("href")).attr("id",target.attr("data-show").substring(1));
-	});
+    //update all the container IDs (so they don't match the url hashes anymore)
+    //ie. #resume becomes #panel-resume
+    $(".navigation-list a").each(function(){
+        var target = $(this);
+        $(target.attr("href")).attr("id",target.attr("data-show").substring(1));
+    });
 
-	//hide all the panels, then show the panel corresponding to the url hash (default to first item)
-	$(".content-container").children().prop("hidden", true);
-	var initialShow;
-	if (window.location.hash) {
-		initialShow = $(".navigation-list a[href=\"" + window.location.hash + "\"]").attr("data-show");
-	} else {
-		initialShow = $(".navigation-list a").first().attr("data-show");
-	}
-	$(initialShow).prop("hidden", false);
+    //hide all the panels, then show the panel corresponding to the url hash (default to first item)
+    $(".content-container").children().prop("hidden", true);
+    var initialShow;
+    if (window.location.hash) {
+        initialShow = $(".navigation-list a[href=\"" + window.location.hash + "\"]").attr("data-show");
+    } else {
+        initialShow = $(".navigation-list a").first().attr("data-show");
+    }
+    $(initialShow).prop("hidden", false);
 
-	//add click events to all the navigation links
-	$(".navigation-list a").click(function(e) {
-		//show the corresponding panel
-		$(".content-container").children().prop("hidden", true);
-		$($(e.target).attr("data-show")).prop("hidden", false);
-		//update the url
-		history.replaceState(
-			null,
-			document.title,
-			window.location.pathname + $(e.target).attr("href")
-		);
-		e.preventDefault();
-	});
+    //add click events to all the navigation links
+    $(".navigation-list a").click(function(e) {
+        //show the corresponding panel
+        $(".content-container").children().prop("hidden", true);
+        $($(e.target).attr("data-show")).prop("hidden", false);
+        //update the url
+        history.replaceState(
+            null,
+            document.title,
+            window.location.pathname + $(e.target).attr("href")
+        );
+        e.preventDefault();
+    });
 
     //load the latest commit info from the github api
     $.ajax({
@@ -40,16 +40,15 @@ $(document).ready( function() {
         dataType: "json",
         success: function(data, textStatus, jqXHR) {
             var commit = data[0];
-			var shaElement = $("#commit-sha");
+            var shaElement = $("#commit-sha");
             shaElement.text(commit.sha.substring(0,7));
             shaElement.attr("href", commit.html_url);
             var commitDate = new Date(commit.commit.committer.date);
             $("#commit-date").text((commitDate.getMonth()+1) + "/" + commitDate.getFullYear());
-			$(".commit-failed-msg").prop("hidden", true);
+            $(".commit-failed-msg").prop("hidden", true);
         },
         error: function(jqXHR, textStatus, errorThrown) {
             $(".commit-live").prop("hidden", true);
         }  
     });
 });
-
--- uploads/index.php
+++ uploads/index.php
@@ -7,20 +7,20 @@ dir_listing($dir);
 //shows a listing for the local directory $dir
 function dir_listing($dir)
 {
-	$files = scandir($dir);
+    $files = scandir($dir);
 
-	$hidden = array(
-		'.',
-		'.htaccess',
-		'index.php',
-	);
-	$files = array_diff($files,$hidden);
-	
-	foreach ($files as $file) {
-		if (explode('.',$file)[1] == 'mp3') {
-			echo('<audio controls><source src="' . $file . '" type="audio/mpeg"></audio> (' . $file . ')<br />');
-		} else {
-			echo('<a href="' . $file . '">' . $file . '</a><br />');
-		}
-	}
+    $hidden = array(
+        '.',
+        '.htaccess',
+        'index.php',
+    );
+    $files = array_diff($files,$hidden);
+    
+    foreach ($files as $file) {
+        if (explode('.',$file)[1] == 'mp3') {
+            echo('<audio controls><source src="' . $file . '" type="audio/mpeg"></audio> (' . $file . ')<br />');
+        } else {
+            echo('<a href="' . $file . '">' . $file . '</a><br />');
+        }
+    }
 }
