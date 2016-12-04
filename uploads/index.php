dfdd9de1759bfa8e0e861ed91abf254475716764
Connor Bracewell <cbrace@uvic.ca>
2016-12-03 17:27:31 -0800

    Made a few more cosmetic changes to files.

--- uploads/index.php
+++ uploads/index.php
@@ -1,26 +1,19 @@
 <?php
 
 //get the requested URL (in local directory format) and show the listing
-$dir = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
-dir_listing($dir);
+$dir = $_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"];
+print_dir_listing($dir);
 
 //shows a listing for the local directory $dir
-function dir_listing($dir)
-{
+function print_dir_listing($dir) {
     $files = scandir($dir);
-
-    $hidden = array(
-        '.',
-        '.htaccess',
-        'index.php',
-    );
-    $files = array_diff($files,$hidden);
-    
+    $hidden = array(".", ".htaccess", "index.php");
+    $files = array_diff($files, $hidden);
     foreach ($files as $file) {
-        if (explode('.',$file)[1] == 'mp3') {
-            echo('<audio controls><source src="' . $file . '" type="audio/mpeg"></audio> (' . $file . ')<br />');
+        if (explode(".", $file)[-1] == "mp3") {
+            echo("<audio controls><source src=\"" . $file . "\" type=\"audio/mpeg\"></audio> (" . $file . ")<br />");
         } else {
-            echo('<a href="' . $file . '">' . $file . '</a><br />');
+            echo("<a href=\"" . $file . "\">" . $file . "</a><br />");
         }
     }
 }
