76a253da991d80e18195e9ee9d1f86190bd9ae06
Connor Bracewell <cbrace@uvic.ca>
2016-12-03 16:54:41 -0800

    Changed element hiding to use "hidden" property.
    Reworked some of the logic used to hide unopened content panels, which should make it easier to work with in the future.
    
     - Changed hiding mechanism from the .hidden class to the html hidden property so that hiding will still be applied if the CSS doesn't load.
    
     - By the way, one of the last few patches closes #3.
    
     - Whitespace issues will be fixed in the next commit.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
 $(document).ready( function() {
-	//hide all the content containers
-	$(".content-container").children().addClass("hidden");
+	//hide fallback content and show script content
+	$(".js-only").prop("hidden", false);
+	$(".nojs-only").prop("hidden", true);
 
-	//update all the container IDs (so they don't match the passed hashes anymore)
+	//update all the container IDs (so they don't match the url hashes anymore)
+	//ie. #resume becomes #panel-resume
 	$(".navigation-list a").each(function(){
-		target = $(this);
+		var target = $(this);
 		$(target.attr("href")).attr("id",target.attr("data-show").substring(1));
 	});
 
-	//show the panel corresponding to the passed hash (default to first item)
-	initialShow = $(".navigation-list a").first().attr("data-show");
-	if (window.location.hash.length != "") {
+	//hide all the panels, then show the panel corresponding to the url hash (default to first item)
+	$(".content-container").children().prop("hidden", true);
+	var initialShow;
+	if (window.location.hash) {
 		initialShow = $(".navigation-list a[href=\"" + window.location.hash + "\"]").attr("data-show");
+	} else {
+		initialShow = $(".navigation-list a").first().attr("data-show");
 	}
-	$(initialShow).removeClass("hidden");
+	$(initialShow).prop("hidden", false);
 
-	//hide the fallback headers
-	$(".fallback-header").each(function(){ $(this).addClass("hidden"); });
-
-	//add click events to all the panel links
-	$("a[data-show]").click( function(e) {
-		$(".content-container").children().addClass("hidden");
-		$($(e.target).attr("data-show")).removeClass("hidden");
+	//add click events to all the navigation links
+	$(".navigation-list a").click(function(e) {
+		//show the corresponding panel
+		$(".content-container").children().prop("hidden", true);
+		$($(e.target).attr("data-show")).prop("hidden", false);
+		//update the url
 		history.replaceState(
 			null,
 			document.title,
@@ -30,20 +34,21 @@ $(document).ready( function() {
 		e.preventDefault();
 	});
 
-    //load the latest commit info
+    //load the latest commit info from the github api
     $.ajax({
         url: "https://api.github.com/repos/connor-bracewell/nnorco/commits?path=index.html",
         dataType: "json",
         success: function(data, textStatus, jqXHR) {
             var commit = data[0];
-            $("#commit-sha").text(commit.sha.substring(0,6));
-            $("#commit-sha").attr("href", commit.html_url);
-            var commitDate = new Date(Date.parse(commit.commit.committer.date));
+			var shaElement = $("#commit-sha");
+            shaElement.text(commit.sha.substring(0,7));
+            shaElement.attr("href", commit.html_url);
+            var commitDate = new Date(commit.commit.committer.date);
             $("#commit-date").text((commitDate.getMonth()+1) + "/" + commitDate.getFullYear());
-            $(".commit-live").removeClass("hidden");
+			$(".commit-failed-msg").prop("hidden", true);
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            $(".commit-failed-msg").removeClass("hidden");
+            $(".commit-live").prop("hidden", true);
         }  
     });
 });
