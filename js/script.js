53be211f68fb5fe05090d202c7c5783c729c1476
Connor Bracewell <cbrace@uvic.ca>
2016-12-06 21:34:46 -0800

    Revamped most of the site markup.
     - Rewrote all the CSS to use em instead of px
     - Changed the site styling slightly (font is now larger).
     - Rewrote and simplified most of the HTML (fixes #2).
     - Rewrote the front page copy (closes #4).
     - Fixed a regression with navigator behavior.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $(document).ready( function() {
-    //hide fallback content and show script content
-    $(".js-only").prop("hidden", false);
-    $(".nojs-only").prop("hidden", true);
+    //Hide NoScript fallback content.
+    $(".noscript-only").prop("hidden", true);
 
-    //update all the container IDs (so they don't match the url hashes anymore)
-    //ie. #resume becomes #panel-resume
+    //Update all the container IDs to match the data-show values instead of URL hashes.
+    //ie. #resume has its ID set to "resume-panel"
     $(".navigation-list a").each(function(){
         var target = $(this);
-        $(target.attr("href")).attr("id",target.attr("data-show").substring(1));
+        var urlHash = target.attr("href");
+        var panelId = target.attr("data-show").substring(1);
+        if (urlHash !== "#") {
+            $(urlHash).attr("id", panelId);
+        }
     });
 
     //hide all the panels, then show the panel corresponding to the url hash (default to first item)
@@ -20,12 +23,12 @@ $(document).ready( function() {
     }
     $(initialShow).prop("hidden", false);
 
-    //add click events to all the navigation links
-    $(".navigation-list a").click(function(e) {
+    //add click events to all the navigation links (including the ones in the body)
+    $("a[data-show]").click(function(e) {
         //show the corresponding panel
         $(".content-container").children().prop("hidden", true);
         $($(e.target).attr("data-show")).prop("hidden", false);
-        //update the url
+        //update the url to match the hash from the anchor
         history.replaceState(
             null,
             document.title,
@@ -34,7 +37,7 @@ $(document).ready( function() {
         e.preventDefault();
     });
 
-    //load the latest commit info from the github api
+    //load the last commit info from the github api
     $.ajax({
         url: "https://api.github.com/repos/connor-bracewell/nnorco/commits?path=index.html",
         dataType: "json",
@@ -45,10 +48,10 @@ $(document).ready( function() {
             shaElement.attr("href", commit.html_url);
             var commitDate = new Date(commit.commit.committer.date);
             $("#commit-date").text((commitDate.getMonth()+1) + "/" + commitDate.getFullYear());
-            $(".commit-failed-msg").prop("hidden", true);
+            $(".last-commit-info").prop("hidden", false);
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            $(".commit-live").prop("hidden", true);
+            $(".fetch-failed-message").prop("hidden", false);
         }  
     });
 });
