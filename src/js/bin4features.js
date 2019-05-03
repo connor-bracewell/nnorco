4031ea3e668502b704e040b856ba39e4d766c028
Connor Bracewell <connor.bracewell@gmail.com>
2019-05-02 17:59:18 -0700

    Update burger page to parse salads

--- src/js/bin4features.js
+++ src/js/bin4features.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(document).ready(function() {
 
     var menuUrl = "https://www.bin4burgerlounge.com/our-downtown-menu/";
     $.ajax({
@@ -11,21 +11,33 @@ $(document).ready(function() {
     function writeData(data, textStatus, jqXHR) {
         var root = $($.parseHTML(data));
         writeItem(root, "burger");
-        writeItem(root, "appy");
+        writeItem(root, "appy", "salad");
         writeItem(root, "feature cocktail");
         writeItem(root, "dessert");
     }
 
     function writeItem(root, name) {
+        writeItem(root, name, null);
+    }
+
+    function writeItem(root, name, alt) {
+        let itemElem = getItemElem(root, name);
+        if (itemElem.length === 0 && alt !== null) {
+            itemElem = getItemElem(root, alt);
+        }
+        name = name.replace(" ","");
+        jQuery("#" + name + "_name").text(itemElem.find(".item-title").text());
+        jQuery("#" + name + "_price").text(itemElem.find(".item-price").text());
+        jQuery("#" + name + "_description").text(itemElem.find("p").text());
+    }
+
+    function getItemElem(root, name) {
         var itemElem = root.find(".slide-features .menu-item")
             .filter(function(index, elem) {
                 var text = jQuery(elem).find(".item-label").first().text();
                 return (text == name);
             }).first();
-        name = name.replace(" ","");
-        jQuery("#" + name + "_name").text(itemElem.find(".item-title").text());
-        jQuery("#" + name + "_price").text(itemElem.find(".item-price").text());
-        jQuery("#" + name + "_description").text(itemElem.find("p").text());
+        return itemElem;
     }
 
 });
