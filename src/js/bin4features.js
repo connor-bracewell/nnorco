cad4fb1a157f79c733252e32a97a043efc1a9637
Connor Bracewell <connor.bracewell@gmail.com>
2019-05-02 18:16:03 -0700

    Fix issue with parsing 'salad's from Bin4 (#16)
    * Fixes an issue where a 'salad' from the Bin4 website is not shown as the featured 'appy'.

--- src/js/bin4features.js
+++ src/js/bin4features.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    var menuUrl = "https://www.bin4burgerlounge.com/our-downtown-menu/";
+    let menuUrl = "https://www.bin4burgerlounge.com/our-downtown-menu/";
     $.ajax({
         url: "https://cors-anywhere.herokuapp.com/" + menuUrl,
         dataType: "html",
@@ -9,23 +9,31 @@ $(document).ready(function() {
     });
 
     function writeData(data, textStatus, jqXHR) {
-        var root = $($.parseHTML(data));
+        let root = $($.parseHTML(data));
         writeItem(root, "burger");
-        writeItem(root, "appy");
+        writeItem(root, "appy", "salad");
         writeItem(root, "feature cocktail");
         writeItem(root, "dessert");
     }
 
-    function writeItem(root, name) {
-        var itemElem = root.find(".slide-features .menu-item")
-            .filter(function(index, elem) {
-                var text = jQuery(elem).find(".item-label").first().text();
-                return (text == name);
-            }).first();
+    function writeItem(root, name, alt) {
+        let itemElem = getItemElem(root, name);
+        if (itemElem.length === 0 && alt !== undefined) {
+            itemElem = getItemElem(root, alt);
+        }
         name = name.replace(" ","");
         jQuery("#" + name + "_name").text(itemElem.find(".item-title").text());
         jQuery("#" + name + "_price").text(itemElem.find(".item-price").text());
         jQuery("#" + name + "_description").text(itemElem.find("p").text());
+    }
+
+    function getItemElem(root, name) {
+        let itemElem = root.find(".slide-features .menu-item")
+            .filter(function(index, elem) {
+                let text = jQuery(elem).find(".item-label").first().text();
+                return (text == name);
+            }).first();
+        return itemElem;
     }
 
 });
