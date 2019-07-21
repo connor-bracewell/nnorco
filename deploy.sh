d9dcfc1063ecaaaf805c4f843e6f3249e733cb90
Connor Bracewell <connor.bracewell@gmail.com>
2019-07-20 17:03:58 -0700

    Add local deploy script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+rm -rf tmp web
+mkdir tmp web
+cat src/data.json
+curl -H \"User-Agent: connor-bracewell\" https://api.github.com/repos/connor-bracewell/nnorco/commits > tmp/commit.json
+cat tmp/commit.json
+jq ".commit_date=\"$(date "+%m/%Y")\"" src/data.json > tmp/data2.json
+cat tmp/data2.json
+jq ".commit_hash=$(cat tmp/commit.json | jq .[0].sha[0:7])" tmp/data2.json > tmp/data3.json
+cat tmp/data3.json
+jq  ".commit_url=$(cat tmp/commit.json | jq .[0].html_url)" tmp/data3.json > tmp/data4.json
+cat tmp/data4.json
+mustache tmp/data4.json src/index.mustache > web/index.html
+cp src/404.html web
+mkdir web/js
+tsc --outFile web/js/main.js src/ts/main.ts
+cp -r src/js/* web/js
+cp -r src/img web
+mkdir web/burger
+cp src/burger.html web/burger/index.html
+mkdir web/css
+sass src/sass/style.scss > web/css/style.css
+sass src/sass/bin4features.scss > web/css/bin4features.css
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  surge web https://nnor.co
+elif [ "$TRAVIS_BRANCH" = "dev" ]; then
+  surge web https://dev.nnor.co
+  surge web nnorco.surge.sh
+fi
