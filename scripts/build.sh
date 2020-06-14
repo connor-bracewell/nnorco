c2e5db621aeda5f2373bd713c7d641abf021048f
Connor Bracewell <connor.bracewell@gmail.com>
2020-06-14 03:12:37 -0700

    Update dependencies and build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,16 +7,16 @@ mkdir tmp web
 cat src/data.json
 curl -u $GITHUB_AUTH_USER:$GITHUB_AUTH_KEY https://api.github.com/repos/connor-bracewell/nnorco/commits > tmp/commit.json
 cat tmp/commit.json
-jq ".commit_date=\"$(date "+%m/%Y")\"" src/data.json > tmp/data2.json
+./node_modules/node-jq/bin/jq ".commit_date=\"$(date "+%m/%Y")\"" src/data.json > tmp/data2.json
 cat tmp/data2.json
-jq ".commit_hash=$(cat tmp/commit.json | jq .[0].sha[0:7])" tmp/data2.json > tmp/data3.json
+./node_modules/node-jq/bin/jq ".commit_hash=$(cat tmp/commit.json | ./node_modules/node-jq/bin/jq .[0].sha[0:7])" tmp/data2.json > tmp/data3.json
 cat tmp/data3.json
-jq  ".commit_url=$(cat tmp/commit.json | jq .[0].html_url)" tmp/data3.json > tmp/data4.json
+./node_modules/node-jq/bin/jq  ".commit_url=$(cat tmp/commit.json | ./node_modules/node-jq/bin/jq .[0].html_url)" tmp/data3.json > tmp/data4.json
 cat tmp/data4.json
 mustache tmp/data4.json src/index.mustache > web/index.html
 cp src/404.html web
 mkdir web/js
-tsc --outFile web/js/main.js src/ts/main.ts
+tsc --lib "es2015,dom" --outFile web/js/main.js src/ts/main.ts
 cp -r src/js/* web/js
 cp -r src/img web
 mkdir web/burger
