a11178e10c26239e0ce360c70d6298f91489aeb9
Connor Bracewell <connor.bracewell@gmail.com>
2023-06-17 15:20:02 -0700

    Only report commits touching /src

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,14 +5,10 @@ set -e # Bail out on first error.
 rm -rf tmp web
 mkdir tmp web
 cat src/data.json
-curl https://api.github.com/repos/connor-bracewell/nnorco/commits?per_page=1 > tmp/commit.json
-cat tmp/commit.json
+curl "https://api.github.com/repos/connor-bracewell/nnorco/commits?path=src&per_page=1" > tmp/commit.json
 jq ".commit_date=\"$(date "+%m/%Y")\"" src/data.json > tmp/data2.json
-cat tmp/data2.json
 jq ".commit_hash=$(cat tmp/commit.json | jq .[0].sha[0:7])" tmp/data2.json > tmp/data3.json
-cat tmp/data3.json
 jq  ".commit_url=$(cat tmp/commit.json | jq .[0].html_url)" tmp/data3.json > tmp/data4.json
-cat tmp/data4.json
 mustache tmp/data4.json src/index.mustache > web/index.html
 cp src/404.html web
 mkdir web/js
